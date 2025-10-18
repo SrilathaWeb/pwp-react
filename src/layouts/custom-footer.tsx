f7dc7e6816c4f3c65268f799799cad4902847df8
Srilatha Alla <salla@cnm.edu>
2025-10-17 20:44:16 -0600

    pwp footer changes

--- src/layouts/custom-footer.tsx
+++ src/layouts/custom-footer.tsx
@@ -4,12 +4,12 @@ export function CustomFooter() {
             <div className="container mx-auto text-center p-4">
                 <p>&copy; {new Date().getFullYear()} My Portfolio. All rights reserved.</p>
                 <div className="mt-4">
-                    <a href=" https://www.linkedin.com/in/srilatha-alla-2b4b6214/" className="mx-2 hover:text-gray-400">LinkedIn</a>
-                    <a href="git hub url"
-                          className="mx-2 hover:text-gray-400">GitHub</a>
-                    <a href="https://twitter.com/SrilathaAlla" className="mx-2 hover:text-gray-400">Twitter</a>
+                    <i className="fa-brands fa-linkedin text-white"></i>
+                    <a href="https://www.linkedin.com/in/srilatha-alla/" className="mx-2 hover:text-gray-400" target='_blank'></a>
+                    <i className="fa-brands fa-github"></i>
+                    <a href="https://github.com/SrilathaCTS" className="mx-2 hover:text-gray-400"  target='_blank'></a>
                     <i className="fa-brands fa-youtube text-red-500"></i>
-                    <a href="https://www.youtube.com/@sathvikreddy16" target="_blank" className="hover:text-cyan-400 ml-1">Subscribe to my Channel</a>
+                    <a href="https://www.youtube.com/@sathvikreddy16" target="_blank" className="hover:text-cyan-400 ml-1"></a>
                 </div>
             </div>
         </footer>
