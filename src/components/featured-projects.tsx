0ce3a82c63cd8b0d37c79d4fd559b61cb1a827d4
Srilatha Alla <salla@cnm.edu>
2025-11-26 17:34:49 -0700

    popup transparent

--- src/components/featured-projects.tsx
+++ src/components/featured-projects.tsx
@@ -191,7 +191,7 @@ const ProjectModal: React.FC<{ project: PortfolioProject | null; onClose: () => 
 
     return (
         <div
-            className="fixed inset-0 bg-black bg-opacity-70 z-50 flex items-center justify-center p-4 transition-opacity duration-300"
+            className="fixed inset-0 bg-transparent bg-opacity-70 z-50 flex items-center justify-center p-4 transition-opacity duration-300"
             onClick={onClose}
         >
             <motion.div
